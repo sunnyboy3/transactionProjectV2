4e4893ce11bf24fc73c3dc0823f0a34b22912b57
wangpeng <wangpeng@jiuaoedu.com>
2022-10-12 18:29:18 +0800

    修改说明

--- JkTools/src/main/java/org/jk/utils/AnnotationUtils.java
+++ JkTools/src/main/java/org/jk/utils/AnnotationUtils.java
@@ -15,26 +15,17 @@ import java.util.List;
 
 public class AnnotationUtils {
     public static List<AnnotationEntity> getRequestMappingValue(String packageName) {
-        //第一个class类的集合
         List<Class<?>> classes = new ArrayList<Class<?>>();
-        //是否循环迭代
         boolean recursive = true;
-        //获取包的名字 并进行替换
         String packageDirName = packageName.replace('.', '/');
-        //定义一个枚举的集合 并进行循环来处理这个目录下的文件
         Enumeration<URL> dirs;
         try {
-            //读取指定package下的所有class
             dirs = Thread.currentThread().getContextClassLoader().getResources(packageDirName);
             while (dirs.hasMoreElements()){
                 URL url = dirs.nextElement();
-                //得到协议的名称
                 String protocol = url.getProtocol();
-                //判断是否以文件的形式保存在服务器上
                 if ("file".equals(protocol)) {
-                    //获取包的物理路径
                     String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
-                    //以文件的方式扫描整个包下的文件 并添加到集合中
                     findAndAddClassesInPackageByFile(packageName, filePath, recursive, classes);
                 }
             }
@@ -43,15 +34,11 @@ public class AnnotationUtils {
         }
         List<AnnotationEntity> annotationList = new ArrayList<AnnotationEntity>();
         for (Class<?> clazz : classes) {
-            //循环获取所有的类
             Class<?> c = clazz;
-            //获取类的所有方法
             Method[] methods = c.getMethods();
             for (Method method : methods) {
-                //获取RequestMapping注解
                 GlobalTransactional annotation = method.getAnnotation(GlobalTransactional.class);
                 if (annotation != null) {
-                    //获取注解的value值
                     annotationList.add(new AnnotationEntity(annotation.name(),annotation.groupName(),annotation.feignClientName()));
                 }
             }
@@ -60,22 +47,16 @@ public class AnnotationUtils {
     }
 
     public static void findAndAddClassesInPackageByFile(String packageName, String packagePath, final boolean recursive, List<Class<?>> classes){
-        //获取此包的目录 建立一个File
         File dir = new File(packagePath);
-        //如果不存在或者 也不是目录就直接返回
         if (!dir.exists() || !dir.isDirectory()) {
             return;
         }
-        //如果存在 就获取包下的所有文件 包括目录
         File[] dirfiles = dir.listFiles(new FileFilter() {
-            //自定义过滤规则 如果可以循环(包含子目录) 或则是以.class结尾的文件(编译好的java类文件)
             public boolean accept(File file) {
                 return (recursive && file.isDirectory()) || (file.getName().endsWith(".class"));
             }
         });
-        //循环所有文件
         for (File file : dirfiles) {
-            //如果是目录 则继续扫描
             if (file.isDirectory()) {
                 findAndAddClassesInPackageByFile(packageName + "." + file.getName(),
                         file.getAbsolutePath(),
@@ -83,10 +64,8 @@ public class AnnotationUtils {
                         classes);
             }
             else {
-                //如果是java类文件 去掉后面的.class 只留下类名
                 String className = file.getName().substring(0, file.getName().length() - 6);
                 try {
-                    //添加到集合中去
                     classes.add(Thread.currentThread().getContextClassLoader().loadClass(packageName + "." + className));
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
