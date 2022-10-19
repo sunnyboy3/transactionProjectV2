29385fa9ae907d334e6d65dd92d88240c2527a63
wangpeng <wangpeng@jiuaoedu.com>
2022-10-19 16:46:31 +0800

    打印span的ID关联关系

--- JkTools/src/main/java/org/jk/core/TraceIdSleuthManagerImpl.java
+++ JkTools/src/main/java/org/jk/core/TraceIdSleuthManagerImpl.java
@@ -2,7 +2,10 @@ package org.jk.core;
 
 import brave.Tracing;
 import brave.internal.baggage.BaggageFields;
+import brave.propagation.TraceContext;
 import org.jk.utils.ApplicationContextUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +18,8 @@ import java.util.List;
  * @Date 2022/10/13 17:17
  **/
 public class TraceIdSleuthManagerImpl implements TraceIdManager{
+
+    private static final Logger logger = LoggerFactory.getLogger(TraceIdSleuthManagerImpl.class);
 
     public static final String TRANSACTION_TRACE_ID = "transaction-trace-id";
 
@@ -52,7 +57,11 @@ public class TraceIdSleuthManagerImpl implements TraceIdManager{
     @Override
     public String getTraceId() {
         Tracing tracing = ApplicationContextUtils.getApplicationContext().getBean(Tracing.class);
-        return tracing.tracer().currentSpan().context().traceIdString();
+        TraceContext context = tracing.tracer().currentSpan().context();
+        logger.info("父级parentSpanId:{}",context.parentId());
+        logger.info("当前currentSpanId:{}",context.spanId());
+        logger.info("当前traceId:{}",context.traceIdString());
+        return context.traceIdString();
     }
 
 }
